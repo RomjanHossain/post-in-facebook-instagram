e1eea5b1d0b76dca477f879f1123d0e08cd41308
RomjanHossain <romjanhossain726526@gmail.com>
2021-02-14 14:09:07 +0600

    this is the alear dialog

--- dialog.py
+++ dialog.py
@@ -1,25 +1,26 @@
 from tkinter import *
 import tkinter as tk
 from PIL import ImageTk, Image
+from tkinter.font import Font
 import time
 
 
 class AlertWindow:
-    new_description = ''
+    new_description = '' 
 
     def __init__(self, master, image_name):
         self.master = master
         self.image_name = image_name
-        self.frame = tk.Frame(self.master)
-        self.img = self.img = Image.open(
-            'production_ready_posts/' + image_name)
+        self.frame = tk.Frame(self.master,)
+        self.img = self.img = Image.open(image_name)
         self.img = self.img.resize((240, 300), Image.ANTIALIAS)
         self.img = ImageTk.PhotoImage(self.img)
         self.panel = tk.Label(master, image=self.img)
-        self.panel.pack(side="top", fill="both", expand="no")
+        self.panel.pack(side="top", fill="both", expand="yes")
         self.L = tk.Label(master, text="Post Description:")
         self.L.pack()
-        self.T = tk.Text(master, height=10, width=40)
+        self.T = tk.Text(master, height=10, width=40) 
+        self.T.config(font=('Courier',14))
         self.T.pack()
         self.B = tk.Button(master,
                            text='Add',
@@ -31,3 +32,8 @@ class AlertWindow:
         self.new_description = self.T.get("1.0", 'end')
         self.master.quit()
         self.master.destroy()
+
+if __name__=='__main__':
+    root=Tk()
+    app=AlertWindow(root,'Image/kinnny.jpg')
+    root.mainloop()
